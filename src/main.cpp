8469c3d2aafb365404a9854894bfefd3951811fd
Pasha Matveev <pasha.matveev.1287@mail.ru>
2024-04-02 18:59:10 +0300

    fixed Serial reading

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 Base base;
 bool state = 1;
 String command = "";
+int count = 0;
 
 void setup()
 {
@@ -34,34 +35,29 @@ void loop()
 {
   if (Serial.available() > 0)
   {
-    command = Serial.readString();
-    Serial.print("SERIAL -> ");
+    // command = Serial.readString();
+    command = Serial.readStringUntil('\n');
     Serial.println(command);
     if (command.startsWith("MOVE"))
     {
       base.speed = command.substring(5, 7).toInt();
       base.direction = command.substring(8, 11).toInt();
-
-      Serial.print("SET Speed to ");
-      Serial.println(base.speed);
-      Serial.print("SET Angle to ");
-      Serial.println(base.direction);
-
       base.move(base.direction, base.speed, 0);
-      delay(1000);
-      base.move(0, 0, 0);
-
-      Serial.println("STOP");
     }
     if (command.startsWith("STOP"))
     {
       base.speed = 0;
       base.direction = 0;
-      Serial.println("STOP");
     }
   }
-  Serial.println('1');
-  digitalWrite(LED_BUILTIN, state);
-  state = !state;
-  delay(1000);
+
+  if (count >= 1000 / 10)
+  {
+    digitalWrite(LED_BUILTIN, state);
+    state = !state;
+    count = 0;
+  }
+  
+  count++;
+  delay(10);
 }
